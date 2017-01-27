b9fe1d69eb919eb01770c230b6a825c686dd80e8
HermantNET <hermant.edu@gmail.com>
2017-01-26 22:32:58 -0500

    bundled for production

--- app/index.js
+++ app/index.js
@@ -1,3 +1,8 @@
 import calculator from './calculator';
 import gallery from './gallery';
 import t3 from './tictactoe';
+
+import styles from './styles/styles.css';
+import css_calculator from './styles/calculator.css';
+import css_gallery from './styles/gallery.css';
+import css_t3 from './styles/t3.css';
