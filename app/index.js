486e414d70cfeb5b7083468c5808fc6efe6c7fc4
HermantNET <hermant.edu@gmail.com>
2017-01-29 19:35:26 -0500

    added doc ready so animations fire properly

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
 import calculator from './calculator';
 import gallery from './gallery';
 import t3 from './tictactoe';
+import ready from 'doc-ready';
 
 import styles from './styles/styles.scss';
 import css_calculator from './styles/calculator.scss';
 import css_gallery from './styles/gallery.scss';
 import css_t3 from './styles/t3.scss';
 
-AOS.init({
-  offset: 300
+ready( function() {
+  AOS.init({
+    offset: 300
+  });
 });
